ced5b35633b583ee169518fa7faafa4df1b35db3
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-09-30 22:20:23 +0530

    Added:comparing two lines

--- src/main/java/org/LineComparision/blz/LineComparision.java
+++ src/main/java/org/LineComparision/blz/LineComparision.java
@@ -2,8 +2,9 @@ package org.LineComparision.blz;
 
 import java.util.Scanner;
 
+
+
 import java.lang.*;
-import java.lang.reflect.Method;
 
 
 public class LineComparision {
@@ -11,9 +12,11 @@ public class LineComparision {
         System.out.println("Welcome to Line Comparison Computation");
 
         int x1, x2, x3, x4, y1, y2, y3, y4;
+        Double lineOne, lineTwo;
 
 
         Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("enter x1 point");
 
         x1 = sc.nextInt();
@@ -44,23 +47,30 @@ public class LineComparision {
         System.out.println("enter y4 point");
 
         y4 = sc.nextInt();
-        double lineOne = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        double lineTwo = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        scanner.close();
+        lineOne = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        lineTwo = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
         System.out.println("The Length OF the  First Line Is " + lineOne);
         System.out.println("The Length OF the  Second Line Is " + lineTwo);
-        String myStr1 = "lineOne";
-        String myStr2 = "lineTwo";
-
-        boolean Answer = myStr1.equals(myStr2);
-
-        if (true) {
-            System.out.println("Both lines are  equal.");
+        int answer = lineOne.compareTo(lineTwo);
+        if (answer > 0) {
+            System.out.println("Line 1 Is Smaller Than Line Two");
+        } else if (answer < 0) {
+            System.out.println("Line 1 Is Greater Than Line Two");
+        } else {
+            System.out.println("Both Lines Are Equal");
         }
-        else {
-            System.out.println("Both lines are not equal.");
-        }
+
+
     }
+
+
 }
+
+
+
+
+
 
 
 
