2e52818ccc2a8009c729ed85a020ac020e4a89a9
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-09-30 12:43:39 +0530

    Added:Checking Equality

--- src/main/java/org/LineComparision/blz/LineComparision.java
+++ src/main/java/org/LineComparision/blz/LineComparision.java
@@ -2,13 +2,17 @@ package org.LineComparision.blz;
 
 import java.util.Scanner;
 
+import java.lang.*;
+import java.lang.reflect.Method;
+
 
 public class LineComparision {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation");
 
-        int x1, x2, y1, y2;
-        double lengthOfALine;
+        int x1, x2, x3, x4, y1, y2, y3, y4;
+
+
         Scanner sc = new Scanner(System.in);
         System.out.println("enter x1 point");
 
@@ -25,9 +29,46 @@ public class LineComparision {
         System.out.println("enter y2 point");
 
         y2 = sc.nextInt();
-        lengthOfALine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("The Length OF the length Of The Line Is " + lengthOfALine);
+        System.out.println("enter x3 point");
 
+        x3 = sc.nextInt();
 
+        System.out.println("enter y3 point");
+
+        y3 = sc.nextInt();
+
+        System.out.println("enter x4 point");
+
+        x4 = sc.nextInt();
+
+        System.out.println("enter y4 point");
+
+        y4 = sc.nextInt();
+        double lineOne = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        double lineTwo = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("The Length OF the  First Line Is " + lineOne);
+        System.out.println("The Length OF the  Second Line Is " + lineTwo);
+        String myStr1 = "lineOne";
+        String myStr2 = "lineTwo";
+
+        boolean Answer = myStr1.equals(myStr2);
+
+        if (true) {
+            System.out.println("Both lines are  equal.");
+        }
+        else {
+            System.out.println("Both lines are not equal.");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
